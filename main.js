371f84667e26e6248a78f11b8c4a3def63c3b850
bahrii96 <81860681+bahrii96@users.noreply.github.com>
2023-01-30 00:00:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-$('.pokemon').click(addPockemon);
-function addPockemon() {
-   let id = $('.id_pokemon').val();
+// $('.pokemon').click(addPockemon);
+// function addPockemon() {
+//    let id = $('.id_pokemon').val();
 
-   $.ajax(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-      success: function (data) {
-         let name = data.forms.forEach(element => {
-            let a = $('.test').after(`<span>${element.name}</span><br>`);
-         });
-         console.log(data);
-      },
-   });
-}
+//    $.ajax(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+//       success: function (data) {
+//          let name = data.forms.forEach(element => {
+//             let a = $('.test').after(`<span>${element.name}</span><br>`);
+//          });
+//          console.log(data);
+//       },
+//    });
+// }
+(function ($) {
+   $('.btn-one').click(addclassone);
+
+   function addclassone() {
+      console.log('qqq');
+      $('p.text').addClass('active');
+      $('.btn-one').css('display', 'none');
+   }
+
+   $('.btn-two').click(addclasstwo);
+
+   function addclasstwo() {
+      $('img').addClass('active');
+      $('.btn-two').css('display', 'none');
+   }
+})(jQuery);
